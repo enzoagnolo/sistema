b62a4e5fbbcaf1069195184e06cc8f637a4fd6d4
enzoagnolo <158178827+enzoagnolo@users.noreply.github.com>
2024-09-16 11:49:49 -0300

    Add files via upload

---
+++ login/src/main/java/com/sesi/login/controller/AutenticacaoController.java
@@ -0,0 +1,13 @@
+package com.sesi.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AutenticacaoController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
